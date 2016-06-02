6c79cbac77f6bf698aec7735202315516cf26824
Fernando Terroso <f.terroso@yahoo.com>
2016-06-02 19:01:53 +0200

    Delete player.js

--- app/player.js
+++
@@ -1,9 +0,0 @@
-function Player(firstName, lastName, score) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.score = score;
-}
-
-Player.prototype.fullName = function fullName() {
-  return this.firstName + ' ' + this.lastName;
-};
